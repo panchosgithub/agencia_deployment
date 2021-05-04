21358daf898c0c54db5b2e432ec7e832b9e7c517
francisco ortiz <panchosdesign@hotmail.com>
2021-05-04 04:04:07 -0700

    Primer Commit

---
+++ index.js
@@ -0,0 +1,54 @@
+import express from 'express';
+import router from './routes/index.js'; 
+import db from './config/db.js';
+
+import dotenv from 'dotenv'; 
+dotenv.config({ path: 'variables.env' });
+
+const app = express();
+
+// Conectar la base de datos
+db.authenticate() 
+    .then( () => console.log('Base de datos conectada') ) 
+    .catch( error => console.log(error));
+
+
+// Puerto y host para la app
+const host = process.env.HOST || '0.0.0.0';
+const port = process.env.PORT || 3000;
+
+app.listen(port, host, () => {
+    console.log('El servidor esta funcionando');
+});
+
+//app.listen(port, () => {
+    //console.log(`El Servidor esta funcionando en el puerto ${port}`)
+//});
+
+
+
+// Habilitar PUG
+app.set('view engine', 'pug'); 
+
+// Obtener el ano actual
+app.use( (req, res, next) => {
+   const year = new Date();
+
+   res.locals.actualYear = year.getFullYear(); 
+   res.locals.nombresitio = "Agencia de Viajes"; 
+    next();  
+});
+
+// Definir la carpeta publica
+app.use(express.static('public'));
+
+// Agregar body parser para leer los datos del formulario
+app.use(express.urlencoded({ extended: true }))
+
+// Agregar Router
+app.use('/', router);
+
+
+
+
+
